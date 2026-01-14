664df6e586cf4eaa6e0d259cdae5b5c93b0904cb
Ralleballebamse <raller@hotmail.se>
2026-01-14 03:31:41 +0100

    Fixed the post that will go into for loop and create all items into posts on website.

--- public/posts.js
+++ public/posts.js
@@ -18,8 +18,11 @@ async function main() {
     console.log(invData);
     console.log(invData.total_inventory_count);
     console.log(invData.descriptions.length);
+    console.log(invData.descriptions[0].name);
     console.log(invData.descriptions[0].icon_url);
     const imgUrl = `https://community.cloudflare.steamstatic.com/economy/image/${invData.descriptions[0].icon_url}`;
+
+
     for (let i = 0; i <= invData.descriptions.length-1; i++) {
         //console.log(invData.descriptions[i].name);
     }
@@ -27,6 +30,23 @@ async function main() {
         "Image:",
         `https://community.cloudflare.steamstatic.com/economy/image/${invData.descriptions[0].icon_url}`
     );
+
+    // Here I am trying to make an real post which later going to end up in the for-loop
+        const postContainer = document.getElementById("posts");
+        const post = document.createElement("div");
+        post.classList.add("post");
+
+        const itemName = document.createElement("h4");
+        itemName.textContent = invData.descriptions[0].name;
+
+        const itemImage = document.createElement("img");;
+        itemImage.src = `https://community.cloudflare.steamstatic.com/economy/image/${invData.descriptions[0].icon_url}`;
+        
+        const itemPrice = document.createElement("h6");
+        itemPrice.textContent = "50Vbux";
+
+        post.append(itemName, itemImage, itemPrice);
+        postContainer.append(post);
 }
 
 main();
