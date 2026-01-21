4bd8f441956435ed0830017d6e73832312069d04
Ralleballebamse <raller@hotmail.se>
2026-01-21 02:53:01 +0100

    Implemented every catch for this to work with fetching from github and then for those who catches error because information isn't stored checks price in steam market. Also excluded some from getting an price, unnessessary items.

--- public/posts.js
+++ public/posts.js
@@ -9,10 +9,10 @@ async function loadfiles(link) {
 }
 
 async function main() {
+    const sleep = (ms) => new Promise((r) => setTimeout(r, ms)); // This is for making an await sleep
     let countItems = 0; // Starts at 1 because list starts at 0
     let totalItemDisplayed = 1000; // 9 items displayed in start
     let arrayAssets = new Array; // Array for avoiding dublications of items
-    let arrayMarketHashNames = new Array; // Array for all market hast names that being send to steam market place to recive lowest value
 
     const steamid = "76561198992052209";
     const steamLink = `/steam?steamid=${steamid}`;
@@ -22,7 +22,7 @@ async function main() {
 
     console.log(invData);
 
-        const res = await fetch(
+    const res = await fetch(
         "https://raw.githubusercontent.com/somespecialone/steam-item-name-ids/refs/heads/master/data/cs2.json",
     );
 
@@ -30,26 +30,37 @@ async function main() {
 
     async function fetchData(marketName, currency) {
         const currencyId = currency === "€" ? 3 : 1;
-        const response = await fetch(
-           ` /api/steam?item_nameid=${itemDB[marketName]}&currency=${currencyId}`
-        );
-        const test = await response.json();
 
-        console.log(test)
-        console.log("1")
-        if (test.length == 0){
-            return("Undefined")
-        }
+        const response = await fetch(
+            ` /api/steam?item_nameid=${itemDB[marketName]}&currency=${currencyId}`
+        );
+
+        const test = await response.json();
 
         let price = [];
 
-        if (currency === "€") {
-            price = test.sell_order_table.split("€")[0].split(">");
-            price = price[price.length - 1] + currency;
-        } else {
-            price = currency + test.sell_order_table.split("$")[1].split("<")[0];
+        try {
+            if (currency === "€") {
+                price = test.sell_order_table.split("€")[0].split(">");
+                price = price[price.length - 1] + currency;
+            } else {
+                price = currency + test.sell_order_table.split("$")[1].split("<")[0];
+            }
+        } catch (err) {
+            for (let z = 0; z < 5; z++) {
+                await sleep(z * 1000);
+                try {
+                    const res = await fetch(
+                        `/api/steam/lowest?name=${encodeURIComponent(marketName)}&currency=${currencyId}`
+                    );
+                    const priceText = (await res.text()).trim();
+                    return (priceText)
+                } catch (err) {
+                    continue;
+                }
+            }
+            return ("Undefined");
         }
-
         return (price)
     }
 
@@ -64,6 +75,7 @@ async function main() {
     });
 
     const normalPostContainer = document.getElementById("posts");
+    const testContainer = document.createElement("div");
 
     loadMoreItems();
 
@@ -80,14 +92,10 @@ async function main() {
         return (inspectLink)
     }
 
-    const testContainer = document.createElement("div");
-
-
     async function sortItemsByHighPriceToLowFunction() {
         displayVisibleOrHidden(normalPostContainer, testContainer)
 
     }
-
 
     function displayVisibleOrHidden(show = showcontainer, hide = hideContainer) {
         if (show.style.visibility === "hidden") {
@@ -98,15 +106,6 @@ async function main() {
             hide.style.visibility = "visible";
         }
     }
-
-    function loadAllMarketHashNames() {
-        let x = 0;
-        for (let z = 0; z <= invData.descriptions.length - 1; z++) {
-            arrayMarketHashNames.push(invData.descriptions[x].market_hash_name);
-            x++;
-        }
-    }
-
 
     async function loadMoreItems() {
         const end = countItems + totalItemDisplayed;
@@ -127,7 +126,18 @@ async function main() {
                 itemImage.src = `https://community.cloudflare.steamstatic.com/economy/image/${invData.descriptions[i].icon_url}`;
 
                 const itemPrice = document.createElement("h6");
-                itemPrice.textContent = `Market value : ${await fetchData(invData.descriptions[i].market_hash_name, "$")}`;
+                if (invData.descriptions[i].tags.some(
+                    tag => tag.category === ("Type") &&
+                        tag.internal_name.startsWith("CSGO_Type_") &&
+                        !tag.internal_name.includes("MusicKit") &&
+                        !tag.internal_name.includes("Collectible") ||
+                        tag.internal_name.startsWith("Type_Hands") ||
+                        tag.internal_name.startsWith("CSGO_Tool_Sticker")
+                )) {
+                    itemPrice.textContent = `Market value : ${await fetchData(invData.descriptions[i].market_hash_name, "$")}`;
+                } else {
+                    itemPrice.textContent = "Market value : Undefined";
+                }
 
                 if (
                     invData.descriptions[i].tags &&
