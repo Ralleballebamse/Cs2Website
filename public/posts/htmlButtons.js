d140acf07d1f07643570c9862c90f0259bfef675
Ralleballebamse <raller@hotmail.se>
2026-02-15 19:45:11 +0100

    Fixed my file sorting. Small changes in code.

--- public/posts/htmlButtons.js
+++ public/posts/htmlButtons.js
@@ -3,6 +3,7 @@ export function initHtmlButtons(deps) {
         steamProfileButtonAll,
         sortItemsByHighPriceToLow,
         sortItemsByLowPriceToHigh,
+        searchForSkins,
         switchToMain,
         priceHighTextArea,
         containers,
